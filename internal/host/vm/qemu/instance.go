6c5320e8049038885282a89b6816856464e4d86e
Manuel de Brito Fontes <aledbf@gmail.com>
2025-12-30 12:38:08 -0300

    Cleanup

--- internal/host/vm/qemu/instance.go
+++ internal/host/vm/qemu/instance.go
@@ -668,7 +668,6 @@ func (q *Instance) buildKernelCommandLine(startOpts vm.StartOpts) string {
 		fmt.Sprintf("-vsock-rpc-port=%d", vsockRPCPort),
 		fmt.Sprintf("-vsock-stream-port=%d", vsockStreamPort),
 		fmt.Sprintf("-vsock-cid=%d", vsockCID),
-		"-debug",
 	}
 	initArgs = append(initArgs, startOpts.InitArgs...)
 
